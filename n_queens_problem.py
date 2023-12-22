989ba05dd0bef6ef35193c32684af8e6bbf65986
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-22 23:23:18 +0300

    Update n_queens_problem.py

--- n_queens_problem.py
+++ n_queens_problem.py
@@ -1,1 +1,12 @@
 
+# n_queens_problem.py
+import cv2
+import numpy as np
+
+def visualize_board(solution):
+    # Tahtayı OpenCV ile görselleştir
+    # (Bu işlev için OpenCV kütüphanesinin yüklü olması gerekir)
+    # Örneğin, renkli bir tahta oluşturabilir ve vezirleri işaretleyebilirsiniz.
+    pass
+
+# Diğer vezir problemi fonksiyonları da eklenebilir.
