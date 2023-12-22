16f79b3f65de57427d973ab46a68771f0febc57f
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-22 23:22:59 +0300

    Update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,1 +1,23 @@
 
+# genetic_algorithm.py
+import random
+
+def initialize_population(population_size, board_size):
+    # Popülasyonu başlat
+    population = []
+    for _ in range(population_size):
+        individual = random.sample(range(board_size), board_size)
+        population.append(individual)
+    return population
+
+def fitness(individual):
+    # Uygunluk fonksiyonu
+    # Vezirler birbirini tehdit etmiyorsa uygunluk daha yüksek olmalı
+    fitness_score = 0
+    for i in range(len(individual)):
+        for j in range(i + 1, len(individual)):
+            if individual[i] != individual[j] and abs(individual[i] - individual[j]) != abs(i - j):
+                fitness_score += 1
+    return fitness_score
+
+# Diğer genetik algoritma fonksiyonları da eklenebilir (seçim, çaprazlama, mutasyon, vb.).
