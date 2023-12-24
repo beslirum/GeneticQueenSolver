1cccc33744a16843e422eb78247461676f791b6a
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-24 16:32:59 +0300

    Update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,9 +1,13 @@
-
 # genetic_algorithm.py
+
+#This file contains functions related to the genetic alorithm.
+
+#Bu dosya genetik algoritma ile ilgili fonksiyonları içerir.
 import random
 
 def initialize_population(population_size, board_size):
-    # Popülasyonu başlat
+    #Initialize the population with random individuals.
+    #Popülasyonu rastgele bireylerle başlatır.
     population = []
     for _ in range(population_size):
         individual = random.sample(range(board_size), board_size)
@@ -11,8 +15,8 @@ def initialize_population(population_size, board_size):
     return population
 
 def fitness(individual):
-    # Uygunluk fonksiyonu
-    # Vezirler birbirini tehdit etmiyorsa uygunluk daha yüksek olmalı
+    #Calculate the fitness score of an individual.
+    #Bir bireyin uygunluk skorunu hesaplar.
     fitness_score = 0
     for i in range(len(individual)):
         for j in range(i + 1, len(individual)):
@@ -20,4 +24,37 @@ def fitness(individual):
                 fitness_score += 1
     return fitness_score
 
-# Diğer genetik algoritma fonksiyonları da eklenebilir (seçim, çaprazlama, mutasyon, vb.).
+def selection(population, num_parents):
+    #Perform selection of parents from the population.
+    #Popülasyondan ebeveyn seçimini gerçekleştirir.
+
+    # Turnuva seçimi veya başka bir seçim yöntemi uygulayabilirsiniz
+    parents = []
+    for _ in range(num_parents):
+        selected = random.choice(population)
+        parents.append(selected)
+    return parents
+
+def crossover(parents, num_offsprings):
+    # Perform crossover to create offspring from parents.
+    #Çaprazlama yaparak ebeveynlerden yeni bireyler oluşturur.
+    offsprings = []
+    for i in range(0, num_offsprings, 2):
+        parent1 = parents[i % len(parents)]
+        parent2 = parents[(i + 1) % len(parents)]
+        crossover_point = random.randint(1, len(parent1) - 1)
+        offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
+        offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
+        offsprings.extend([offspring1, offspring2])
+    return offsprings
+
+def mutation(individual, mutation_rate):
+    #Perform mutation on an individual.
+    #Bir birey üzerinde mutasyon gerçekleştirir.
+    mutated_individual = individual.copy()
+    for i in range(len(mutated_individual)):
+        if random.random() < mutation_rate:
+            mutated_individual[i] = random.randint(0, len(mutated_individual) - 1)
+    return mutated_individual
+
+# Diğer genetik algoritma fonksiyonları da eklenebilir (örneğin, genetik çeşitlilik artırma, elitizm, vb.).
