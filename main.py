617be174fa56e812a97491650a124529f71297f6
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-24 16:32:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-
 # main.py
-from genetic_algorithm import initialize_population, fitness
+
+#This file is the main entry point for the project.
+#Bu dosya, projenin ana giriş noktasını içerir.
+
+from genetic_algorithm import initialize_population, fitness, selection, crossover, mutation
 from n_queens_problem import visualize_board
-from config import population_size, board_size, generations
-from visualization import create_colored_board 
+from config import population_size, board_size, generations, num_parents, num_offsprings, mutation_rate
 
-
-def genetic_algorithm_solver(population_size, board_size, generations):
-    # Genetik algoritma çözümü
+def genetic_algorithm_solver():
+    #Solve the N Queen problem using a genetic algorithm.
+    #Genetik algoritma kullanarak N Vezir problemini çözer.
+    
     population = initialize_population(population_size, board_size)
     
     for generation in range(generations):
-        # Uygunluk skorlarına göre popülasyonu sırala
         population = sorted(population, key=lambda x: fitness(x), reverse=True)
+        parents = selection(population, num_parents)
+        offsprings = crossover(parents, num_offsprings)
+        population = parents + offsprings
+        population = [mutation(ind, mutation_rate) for ind in population]
 
-        # Seçim, çaprazlama, mutasyon işlemleri uygula
-
-    # En iyi çözümü al ve görselleştir
-    best_solution = population[0]
+    best_solution = max(population, key=fitness)
     visualize_board(best_solution)
 
 if __name__ == "__main__":
-    # Proje ayarları
-    population_size = 100
-    board_size = 8
-    generations = 1000
-
-    genetic_algorithm_solver(population_size, board_size, generations)
+    genetic_algorithm_solver()
